82244d7c4d0188c51d62b918f0af51a6edf84d68
Aftab Nadaf <nadafaftab97@gmail.com>
2024-12-01 17:55:27 +0530

    Made little bit improvements in logic.

--- ARRAYS/HARD/FindRepeatingAndMissingNumber.java
+++ ARRAYS/HARD/FindRepeatingAndMissingNumber.java
@@ -2,7 +2,7 @@ package ARRAYS.HARD;
 
 import java.util.ArrayList;
 
-class FindRepeatingAndMissingNumber {
+public class FindRepeatingAndMissingNumber {
 
     // Approach 0: (Brute) This is brute force solution where I am first looping
     // from 1 to n, then I am looping on given array, finding out the number of
@@ -11,6 +11,7 @@ class FindRepeatingAndMissingNumber {
     // missing number. Since I am using 2 nested loop so the time complexity of this
     // solution would be O(n^2) and SC will be O(1) becuase no extra space is being
     // used here.
+
     public static ArrayList<Integer> findRepeatingMissingNumbersBrute(int[] arr) {
         int n = arr.length;
         int missingElement = -1;
@@ -21,8 +22,8 @@ class FindRepeatingAndMissingNumber {
         for (int i = 1; i <= n; i++) {
             int count = 0;
 
-            for (int j = 0; j < n; j++) {
-                if (arr[j] == i) {
+            for (int k : arr) {
+                if (k == i) {
                     count++;
                 }
             }
@@ -48,13 +49,14 @@ class FindRepeatingAndMissingNumber {
     // means that element is repeating and if value is 0 then that element is
     // missing. The SC of this solution would be O(n) becuase I am using hash of N+1
     // and TC will be O(2n) which is equivalent to O(n).
+
     public static ArrayList<Integer> findRepeatingMissingNumbersBetter(int[] arr) {
 
         int n = arr.length;
         int[] hash = new int[n + 1];
 
-        for (int i = 0; i < n; i++) {
-            hash[arr[i]]++;
+        for (int j : arr) {
+            hash[j]++;
         }
 
         int missingElement = -1;
