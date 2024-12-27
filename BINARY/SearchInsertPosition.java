1bcf5b0e8a336e2ebb6ea2d2d02e51cde120ec04
Aftab Nadaf <nadafaftab97@gmail.com>
2024-12-27 23:03:43 +0530

    This is my todays problem, where I am determining the insert postion of given K element. Right now I just have brute solution for this problem.

---
+++ BINARY/SearchInsertPosition.java
@@ -0,0 +1,26 @@
+package BINARY;
+
+public class SearchInsertPosition {
+
+    // Approach 01 : (Brute) This solution is utilizing the brute force approach
+    // where I am just checking whether the next element is greater than or equal to
+    // given element 'k'. If it does then that ith postion will be our insert
+    // position.
+    public static int searchInsertPosition(int[] arr, int k) {
+        int i = 0;
+
+        while (i < arr.length) {
+            if (arr[i] >= k) {
+                return i;
+            }
+            i++;
+        }
+        return arr.length;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 5, 6 };
+        System.out.println(searchInsertPosition(arr, 7));
+
+    }
+}
